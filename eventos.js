cd73ee1c0cdf0c825b07b466ea379e1b009b441e
Lucian Gallardo <69860953+lucian17-26@users.noreply.github.com>
2022-05-13 12:07:38 -0300

    modificación de DOM

--- eventos.js
+++ eventos.js
@@ -1,6 +1,9 @@
 const cantidadDeCuotas = [3, 6, 9, 12,18,24];
 const iva = (1 * 40) / 100;
 
+
+
+
 usuario.addEventListener("change", () =>{
     agregarUsuarioALocalStorage(usuario.value);
 })
@@ -13,11 +16,10 @@ function agregarUsuarioALocalStorage(usuario){
 
 let montoPrestamo = document.getElementById("monto-prestamo");
 montoPrestamo.addEventListener("change", () =>{
+    montoPrestamo.value == "" ? montoPrestamo.value = "Campo Obligatorio" :
     agregarMontoALocalStorage(montoPrestamo.value)
 } ) 
-// function recibirMonto(){
-//     console.log(montoPrestamo.value);
-// }
+
 function agregarMontoALocalStorage(montoPrestamo){
     localStorage.setItem(`monto`, montoPrestamo)
     console.log(localStorage.getItem(`monto`));
@@ -41,7 +43,6 @@ cuotaPrestamo.append(optionNula);
 
 // Recorre el array CantidadDeCuotas para asignarlas a select y poder definir un numero de cuota 
 cantidadDeCuotas.forEach((cuota) => {
-
     let option = document.createElement("option");
     option.value = cuota;
     option.innerText = `${cuota} Meses`;
@@ -50,19 +51,34 @@ cantidadDeCuotas.forEach((cuota) => {
 });
 
 
-let prestamo = document.getElementById("Boton-1");
+let botnCalcular = document.getElementById("Boton-1");
 
-prestamo.addEventListener("click", calcularPrestamos);
-
-function calcularPrestamos(){
+botnCalcular.addEventListener("click", devolverValor ) 
+function devolverValor(){
+    
+    // funcion a ejecutar
     let salida = (parseInt(montoPrestamo.value) * iva) + parseInt(montoPrestamo.value);
     let salida2 = salida / parseInt(cuotaPrestamo.value);
-    if(salida2 == "" || montoPrestamo.value == "" || cuotaPrestamo.value == ""){
-        alert("Datos mal ingresados");
-    }else{
-        confirm( "Su cuota final es de " +"$"+ Math.ceil(salida2));
-    }
-}
+    let salida3 = Math.ceil(salida2);//salida final de la funcion 
+    
+    // Cuadro descriptivo del prestamo
+    let valorCuota1= document.getElementById("valor-cuota1");
+    valorCuota1.innerText= "Cuota a pagar";
+    let valorCuota= document.getElementById("valor-cuota");
+    valorCuota.innerText= `$ ${salida3}`;
+    
+    let capitalTotal= document.getElementById("capital-total");
+    capitalTotal.innerHTML="$ "+ parseInt(cuotaPrestamo.value)* salida3;
+    let capitalTotal1= document.getElementById("capital-total1");
+    capitalTotal1.innerText= "Total a pagar";
+    
+    let interesAgregado= document.getElementById("interes-agregado");
+    interesAgregado.innerText= "40% IVA ";
+    let interesAgregado1= document.getElementById("interes-agregado1");
+    interesAgregado1.innerText= "Interes agregado";
+    
+};
+
 
 localStorage.setItem(`cantidad_de_cuotas`, cantidadDeCuotas);
 localStorage.setItem(`iva`, iva);
